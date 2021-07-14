ccb98d9d9cc16de11c4927df0ec4cde5fbb47d12
Vladimir Loginov <vladimir.loginov@intel.com>
2021-07-14 18:20:59 +0300

    optional 1 channel first conv

--- misc/pytorch_toolkit/text_recognition/text_recognition/models/backbones/mobilenet.py
+++ misc/pytorch_toolkit/text_recognition/text_recognition/models/backbones/mobilenet.py
@@ -9,11 +9,14 @@ architectures = {
 
 
 class MobileNet(nn.Module):
-    def __init__(self, arch):
+    def __init__(self, arch, in_channels=1):
         super().__init__()
         assert arch in architectures.keys(), f'{arch} not in possible options: {architectures.keys()}'
         model = architectures[arch](pretrained=True)
         self.features = model.features
+        if in_channels == 1:
+            first_conv = self.features[0][0]
+            self.features[0][0] = nn.Conv2d(1, first_conv.out_channels, first_conv.kernel_size, first_conv.stride, first_conv.padding, False)
 
     def forward(self, x):
         x = self.features(x)
